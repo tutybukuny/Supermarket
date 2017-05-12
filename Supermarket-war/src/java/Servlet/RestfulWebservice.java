8b87f3c27d2d2e71a438f0795cc494f2e2b6a438
trung <thanhtrungnguyen1208@gmail.com>
2017-05-12 11:16:00 +0700

    trung

--- Supermarket-war/src/java/Servlet/RestfulWebservice.java
+++ Supermarket-war/src/java/Servlet/RestfulWebservice.java
@@ -5,9 +5,8 @@
  */
 package Servlet;
 
-import entities.Account;
-import entities.Human;
-import entities.Product;
+import entities.*;
+import java.util.ArrayList;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.ejb.LocalBean;
@@ -27,13 +26,17 @@ import sessionbean.*;
 @LocalBean
 @Path("/service")
 public class RestfulWebservice {
-    
+
     @EJB
     HumanFacadeLocal humanDAO;
     @EJB
     AccountFacadeLocal accountDAO;
     @EJB
     ProductFacadeLocal productDAO;
+    @EJB
+    CartFacadeLocal cartDAO;
+    @EJB
+    SelectedproductFacadeLocal selectedproductDAO;
 
     public RestfulWebservice() {
     }
@@ -46,10 +49,10 @@ public class RestfulWebservice {
 
         if (action.equals("login")) {
             return login(req);
-        }else if(action.equals("getAllProducts")){
+        } else if (action.equals("getAllProducts")) {
             return getAllProducts(req);
-        }else if (action.equals("addProductToCart")){
-            return addProductToCart(req);
+        } else if (action.equals("sendCart")) {
+            return addCart(req);
         }
 
         return "{error:\"Something went wrong\"}";
@@ -60,54 +63,79 @@ public class RestfulWebservice {
         JSONObject json = req.getJSONObject("json");
         acc.setUsername(json.getString("username"));
         acc.setPassword(json.getString("password"));
-        
+
         Human human = humanDAO.getHumanByAccount(acc);
-        String jsonResult = "error";
-        
-        if (human != null) {
-            jsonResult = "succes";
-        }
-        
         JSONObject jobj = new JSONObject();
-        jobj.put("result", jsonResult);
+
+        if (human != null) {
+            jobj.put("result", "succes");
+            jobj.put("humanId", human.getId().intValue());
+        } else {
+            jobj.put("result", "error");
+        }
+
         return jobj.toString();
     }
 
     private String getAllProducts(JSONObject req) {
         JSONArray jsArrayProducts = new JSONArray();
         List<Product> list = productDAO.findAll();
-        
-        for(Product p : list){
+
+        for (Product p : list) {
             JSONObject jsObjProduct = new JSONObject();
             jsObjProduct.put("id", p.getId().intValue());
             jsObjProduct.put("name", p.getName());
             jsObjProduct.put("description", p.getDescription());
             jsObjProduct.put("cost", p.getCost().floatValue());
-            
+
             JSONObject jsPreview = new JSONObject();
             jsPreview.put("id", p.getPreviewID().getId().intValue());
             jsPreview.put("intro", p.getPreviewID().getIntro());
             jsPreview.put("image", p.getPreviewID().getImage());
             jsObjProduct.put("preview", jsPreview);
-            
+
             JSONObject jsManufacturer = new JSONObject();
             jsManufacturer.put("id", p.getManufacturerID().getId().intValue());
             jsManufacturer.put("name", p.getManufacturerID().getName());
             jsManufacturer.put("des", p.getManufacturerID().getName());
             jsObjProduct.put("manufacturer", jsManufacturer);
-            
+
             jsArrayProducts.put(jsObjProduct);
         }
-        
+
         JSONObject parameters = new JSONObject();
         parameters.put("products", jsArrayProducts);
         return parameters.toString();
     }
 
-    private String addProductToCart(JSONObject req) {
+    private String addCart(JSONObject req) {
+
+        Cart cart = new Cart();
+        cart.setSelectedproductList(new ArrayList<Selectedproduct>());
+
+        JSONObject cartObj = req.getJSONObject("cart");
+        float totalCost = (float) cartObj.getDouble("totalCost");
+        JSONArray spArray = cartObj.getJSONArray("selectproducts");
+
+        for (int i = 0; i < spArray.length(); i++) {
+            Selectedproduct sp = new Selectedproduct();
+            JSONObject jsSp = spArray.getJSONObject(i);
+            int productId = jsSp.getInt("productId");
+            Product product = productDAO.find(productId);
+            sp.setProductID(product);
+            sp.setCartID(cart);
+            cart.getSelectedproductList().add(sp);
+        }
+
+        JSONObject humanObj = cartObj.getJSONObject("human");
+        int humanId = humanObj.getInt("humanId");
+        Human human = humanDAO.find(humanId);
+        cart.setHumanID(human);
+        cart.setTotalCost(totalCost);
+        cartDAO.create(cart);
+
         JSONObject parameters = new JSONObject();
-        JSONObject json = req.getJSONObject("json");
-        
+        parameters.put("result", "ok");
         return parameters.toString();
     }
 }
